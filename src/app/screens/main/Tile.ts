6cc4fb7853e95c4395201a76ddb17dfabff00c93
Thomas Roncin <thomas.roncin@sencrop.com>
2025-09-06 14:39:18 +0200

    dosc

--- src/app/screens/main/Tile.ts
+++ src/app/screens/main/Tile.ts
@@ -1,6 +1,10 @@
 import { maxBy } from "lodash";
 import { Assets, Container, Sprite, Texture } from "pixi.js";
 
+/**
+ * Neighbors of a tile
+ * false means there is a tile, true means there is nothing (kinda backward)
+ */
 type Neighbors = {
   up: boolean;
   north: boolean;
@@ -10,6 +14,9 @@ type Neighbors = {
   down: boolean;
 };
 
+/**
+ * The name of the 12 quadrants of a tile
+ */
 const quadrantVariants = [
   "11",
   "12",
@@ -25,6 +32,10 @@ const quadrantVariants = [
   "34",
 ] as const;
 type QuadrantVariant = (typeof quadrantVariants)[number];
+
+/**
+ * The quadriant is hidden if all these conditions are met (their is a neighbor in each direction)
+ */
 const quadrantVariantVisibilityConditions: Record<
   QuadrantVariant,
   (keyof Neighbors)[]
@@ -43,14 +54,44 @@ const quadrantVariantVisibilityConditions: Record<
   "34": ["east"],
 };
 
+/**
+ * The X,Y position of each quadrant
+ */
+const quadrantPosition: Record<QuadrantVariant, { x: number; y: number }> = {
+  "11": { x: 0, y: 0 },
+  "12": { x: 8, y: 0 },
+  "13": { x: 16, y: 0 },
+  "14": { x: 24, y: 0 },
+  "21": { x: 0, y: 8 },
+  "22": { x: 8, y: 8 },
+  "23": { x: 16, y: 8 },
+  "24": { x: 24, y: 8 },
+  "31": { x: 0, y: 16 },
+  "32": { x: 8, y: 16 },
+  "33": { x: 16, y: 16 },
+  "34": { x: 24, y: 16 },
+};
+
+/**
+ * A tile
+ */
 export class Tile extends Container {
   constructor({
     type,
     neighbors,
     z,
   }: {
+    /**
+     * the type, ex: wall or stone
+     */
     type: string;
+    /**
+     * the neighbors of the tile
+     */
     neighbors: Neighbors;
+    /**
+     * The Z coordinate of the tile
+     */
     z: number;
   }) {
     super();
@@ -72,21 +113,10 @@ export class Tile extends Container {
   }
 }
 
-const quadrantPosition: Record<QuadrantVariant, { x: number; y: number }> = {
-  "11": { x: 0, y: 0 },
-  "12": { x: 8, y: 0 },
-  "13": { x: 16, y: 0 },
-  "14": { x: 24, y: 0 },
-  "21": { x: 0, y: 8 },
-  "22": { x: 8, y: 8 },
-  "23": { x: 16, y: 8 },
-  "24": { x: 24, y: 8 },
-  "31": { x: 0, y: 16 },
-  "32": { x: 8, y: 16 },
-  "33": { x: 16, y: 16 },
-  "34": { x: 24, y: 16 },
-};
-
+/**
+ * A quadrant
+ * A tile is made of 12 quadrants (3 lines, 4 columns)
+ */
 class Quadrant extends Sprite {
   constructor({
     type,
@@ -102,29 +132,29 @@ class Quadrant extends Sprite {
     tile: Tile;
   }) {
     const neighborsString = neighborsToString(neighbors);
+    // find every textures for this type of tile into the cache of Pixi
+    // TODO: use another system to register the textures instead of using Pixi's cache
     // @ts-expect-error idgaf
     const cache = Assets.cache._cache as Map<string, Texture>;
     const quadrantTextures = [...cache.keys()].filter((k) =>
       k.startsWith(`${type}-${variant}-`)
     );
-    const texturesScores = quadrantTextures.map((textureName) => {
-      const regex = /^([a-z]+)-(\d+)-([a-z]+)(?:_(\d+))?\.png$/;
-      const [, , , n, h] = textureName.match(regex)!;
-      const score = n
-        .split("")
-        .filter((c) => neighborsString.includes(c)).length;
-      const isValidHeight = h ? (z % 2) + 1 === Number(h) : true;
-      const isValid = score === n.length && isValidHeight;
-      return { textureName, score: isValid ? score : -1 };
-    });
+    // score all the textures
+    const texturesScores = quadrantTextures.map((textureName) =>
+      scoreTexture({
+        neighborsString,
+        textureName,
+        z,
+      })
+    );
 
+    // find the best texture for this quadrant
     const best = maxBy(texturesScores, "score")!;
-    console.log({ variant, neighborsString, texturesScores, best });
-
     const texture = Texture.from(best.textureName);
     if (!texture) {
       throw new Error(`Texture not found: "${best.textureName}"`);
     }
+    // keep pixel art style
     texture.source.scaleMode = "nearest";
     const position = quadrantPosition[variant];
 
@@ -134,6 +164,9 @@ class Quadrant extends Sprite {
   }
 }
 
+/**
+ * Serialize the neighbors into a "uneswd" string
+ */
 const neighborsToString = (neighbors: Neighbors): string => {
   const sides = [];
   if (neighbors.up) sides.push("u");
@@ -143,4 +176,25 @@ const neighborsToString = (neighbors: Neighbors): string => {
   if (neighbors.west) sides.push("w");
   if (neighbors.down) sides.push("d");
   return sides.join("");
+};
+
+/**
+ * Score a texture for a quadrant
+ * The higher the score the best this texture fit the neighbors of this quadrant
+ */
+const scoreTexture = ({
+  textureName,
+  neighborsString,
+  z,
+}: {
+  textureName: string;
+  neighborsString: string;
+  z: number;
+}) => {
+  const regex = /^([a-z]+)-(\d+)-([a-z]+)(?:_(\d+))?\.png$/;
+  const [, , , n, h] = textureName.match(regex)!;
+  const score = n.split("").filter((c) => neighborsString.includes(c)).length;
+  const isValidHeight = h ? (z % 2) + 1 === Number(h) : true;
+  const isValid = score === n.length && isValidHeight;
+  return { textureName, score: isValid ? score : -1 };
 };
